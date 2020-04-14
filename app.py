acf40205004e7487ef5348dde337cc383a75c44f
Iryna Korshunovych <irenekor87@gmail.com>
2020-04-14 09:05:36 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# Dependencies and Setup
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Flask Setup
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Root Route to Query MongoDB & Pass Mars Data Into HTML Template: index.html to Display Data
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+# Scrape Route to Import `scrape_mars.py` Script & Call `scrape` Function
+@app.route("/scrape")
+def scrapper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
+
+# Define Main Behavior
+if __name__ == "__main__":
+    app.run()
